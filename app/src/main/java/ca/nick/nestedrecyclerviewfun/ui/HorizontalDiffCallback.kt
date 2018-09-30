9f322ba4f60085e5b6065bb7acc9f483e1d228d2
Nick Rose <nicholas.rose@cbc.ca>
2018-09-30 16:51:17 -0400

    Improve param names for diff callbacks

--- app/src/main/java/ca/nick/nestedrecyclerviewfun/ui/HorizontalDiffCallback.kt
+++ app/src/main/java/ca/nick/nestedrecyclerviewfun/ui/HorizontalDiffCallback.kt
@@ -4,11 +4,6 @@ import androidx.recyclerview.widget.DiffUtil
 
 object HorizontalDiffCallback : DiffUtil.ItemCallback<Int>() {
 
-    override fun areItemsTheSame(p0: Int, p1: Int): Boolean {
-        return p0 == p1
-    }
-
-    override fun areContentsTheSame(p0: Int, p1: Int): Boolean {
-        return p0 == p1
-    }
+    override fun areItemsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
 }
--- app/src/main/java/ca/nick/nestedrecyclerviewfun/ui/VerticalDiffCallback.kt
+++ app/src/main/java/ca/nick/nestedrecyclerviewfun/ui/VerticalDiffCallback.kt
@@ -4,11 +4,6 @@ import androidx.recyclerview.widget.DiffUtil
 
 object VerticalDiffCallback : DiffUtil.ItemCallback<List<Int>>() {
 
-    override fun areItemsTheSame(p0: List<Int>, p1: List<Int>): Boolean {
-        return p0 == p1
-    }
-
-    override fun areContentsTheSame(p0: List<Int>, p1: List<Int>): Boolean {
-        return p0 == p1
-    }
+    override fun areItemsTheSame(oldItem: List<Int>, newItem: List<Int>) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: List<Int>, newItem: List<Int>) = oldItem == newItem
 }
